376965a0d4d366628d3ef5d36f8d047b1d8d4e8f
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-06-03 20:05:16 +0300

    Static folder in build directory?

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use("/api/v1/transactions", transactions);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) =>
+  app.get("/", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
