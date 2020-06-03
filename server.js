8970730df8f340087c9e8b8bf4481de8312ca8b0
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-06-03 21:16:54 +0300

    Fixed bug in server.js

--- server.js
+++ server.js
@@ -21,19 +21,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-console.log(process.env.NODE_ENV);
 // Init morgan
-if ((process.env.NODE_ENV = "development")) app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 // Set api routes
 app.use("/api/v1/transactions", transactions);
 
-// Use static for production
-app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-);
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+// Use static for production
 
 // Get the port
 const PORT = process.env.PORT || 5000;
