1982cc547f47b8ec04a7aa9d4e8d255ef0af2a6d
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-06-03 20:08:33 +0300

    build folder

---
+++ client/build/precache-manifest.4bb7adfc77315133e1dacda2e1d9e095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b691706633d16fbeeae3e7e039df2bb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b23c761d5d209835c2c5",
+    "url": "/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "be49cf117f4930ff2f00",
+    "url": "/static/js/2.b2dfdb28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b2dfdb28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23c761d5d209835c2c5",
+    "url": "/static/js/main.bb937c79.chunk.js"
+  },
+  {
+    "revision": "3f06bada26362405c02f",
+    "url": "/static/js/runtime-main.cc722053.js"
+  }
+]);
